a73c86d1eecd7ed4aced37b1995c514d29ae4a71
ChenYiChu <stitch000000@gmail.com>
2019-11-10 14:42:20 +0800

    w09

---
+++ W09 1N-mid-prep/p2/div_208410398.js
@@ -0,0 +1,26 @@
+function changePicture(index) {
+  var p = document.getElementById("section");
+  switch (index) {
+    case 1:
+      p.innerHTML = '<img src="img/img1.jpg" width=100% height=100% />';
+      break;
+    case 2:
+      p.innerHTML = '<img src="img/img2.jpg" width=100% height=100% />';
+      break;
+    case 3:
+      p.innerHTML = '<img src="img/img3.jpg" width=100% height=100% />';
+      break;
+    case 4:
+      p.innerHTML = '<img src="img/img4.jpg" width=100% height=100% />';
+      break;
+    case 5:
+      p.innerHTML = '<img src="img/img5.jpg" width=100% height=100% />';
+      break;
+    case 6:
+      p.innerHTML = '<img src="img/img6.jpg" width=100% height=100% />';
+      break;
+    case 7:
+      p.innerHTML = '<img src="img/img7.jpg" width=100% height=100% />';
+      break;
+  }
+}
